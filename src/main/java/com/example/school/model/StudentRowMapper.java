58fbfde81e51de0801de14750de86a1c175d0d43
Adarsh <attivillivardhan@gmail.com>
2024-05-24 17:02:44 +0000

    commit message

--- src/main/java/com/example/school/model/StudentRowMapper.java
+++ src/main/java/com/example/school/model/StudentRowMapper.java
@@ -7,12 +7,11 @@ import java.sql.SQLException;
 import com.example.school.model.Student;
 
 public class StudentRowMapper implements RowMapper<Student> {
-public Student mapRow(ResultSet rs, int rowNum) throws SQLException{
-return new Student(
-rs.getInt("studentId"),
-rs.getString("studentName"),
-rs.getString("gender"),
-rs.getInt("standard")
-);
-}
+    public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Student(
+                rs.getInt("studentId"),
+                rs.getString("studentName"),
+                rs.getString("gender"),
+                rs.getInt("standard"));
+    }
 }
